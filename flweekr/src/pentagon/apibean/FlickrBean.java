7e7a1fbc9646e6cce30d7eecc9b4da55f4778ea9
Hao Ge <gehao868@gmail.com>
2014-02-06 17:04:01 -0500

    hge

--- flweekr/src/pentagon/apibean/FlickrBean.java
+++ flweekr/src/pentagon/apibean/FlickrBean.java
@@ -12,5 +12,47 @@ public class FlickrBean {
     	return flickrBaseUrl;
     }
     
+    public String getMethod(){
+    	return flickrMethod;
+    }
     
+    public String getApiKey(){
+    	return flickrAPIKey;
+    }
+    
+    public String getPerPage(){
+    	return flickrPerPage;
+    }
+    
+    public String getFormat(){
+    	return flickrFormat;
+    }
+    
+    public String getTags(){
+    	return flickrTags;
+    }
+    
+    public void setBaseUrl(String s){
+    	flickrBaseUrl = s;
+    }
+    
+    public void setMethod(String s){
+    	flickrMethod = s;
+    }
+    
+    public void setAPIKey(String s){
+    	flickrAPIKey = s;
+    }
+    
+    public void setPerPage(String s){
+    	flickrPerPage = s;
+    }
+    
+    public void setFormat(String s){
+    	flickrFormat = s;
+    }
+    
+    public void setTags(String s){
+    	flickrTags = s;
+    }
 }
