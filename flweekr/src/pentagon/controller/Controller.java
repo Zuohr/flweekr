43a57b7398f66a7543adb0bb7d97bb2d8dca48c8
Zuohr <haoran.zuo@gmail.com>
2014-02-06 17:03:21 -0500

    add framework

--- flweekr/src/pentagon/controller/Controller.java
+++ flweekr/src/pentagon/controller/Controller.java
@@ -1,11 +1,15 @@
 package pentagon.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import pentagon.action.Action;
+import pentagon.action.ActionMap;
 
 /**
  * Servlet implementation class Controller
@@ -13,6 +17,13 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("*.do")
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private final String jspPath = "/WEB-INF/";
+	private ActionMap actions;
+
+	@Override
+	public void init() throws ServletException {
+		actions = new ActionMap();
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
@@ -29,7 +40,38 @@ public class Controller extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-		// weicheng comment
+		try {
+			String nextStep = processRequest(request);
+			proceedToNext(nextStep, request, response);
+		} catch (Exception e) {
+			throw new RuntimeException(e.getCause());
+		}
+	}
+
+	private String processRequest(HttpServletRequest request) {
+		String actionName = getActionName(request.getServletPath());
+		Action action = actions.getAction(actionName);
+		if (action == null) {
+			return "404";
+		} else {
+			return action.perform(request);
+		}
+	}
+
+	private void proceedToNext(String nextStep, HttpServletRequest request,
+			HttpServletResponse response) throws IOException, ServletException {
+		if (nextStep.endsWith(".do")) {
+			response.sendRedirect(nextStep);
+		} else if (nextStep.endsWith(".jsp")) {
+			request.getRequestDispatcher(jspPath + nextStep).forward(request,
+					response);
+		} else {
+			response.sendError(404);
+		}
+	}
+
+	private String getActionName(String path) {
+		return path.substring(path.lastIndexOf('/') + 1);
 	}
 
 }
