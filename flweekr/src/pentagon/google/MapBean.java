dc827483f7aac9eeee406f44c19918dfa2dd640b
Zuohr <haoran.zuo@gmail.com>
2014-02-10 20:24:33 -0500

    add map pack

---
+++ flweekr/src/pentagon/google/MapBean.java
@@ -0,0 +1,32 @@
+package pentagon.google;
+
+public class MapBean {
+	private double latitude;
+	private double longitude;
+	private String imgURL;
+
+	public double getLatitude() {
+		return latitude;
+	}
+
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+
+	public double getLongitude() {
+		return longitude;
+	}
+
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+
+	public String getImgURL() {
+		return imgURL;
+	}
+
+	public void setImgURL(String imgURL) {
+		this.imgURL = imgURL;
+	}
+	
+}
