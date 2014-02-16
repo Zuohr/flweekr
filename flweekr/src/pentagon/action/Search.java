8857173eebede592c8735ba3827734b8ead48700
Zuohr <haoran.zuo@gmail.com>
2014-02-16 02:00:01 -0500

    add lazy loading

--- flweekr/src/pentagon/action/Search.java
+++ flweekr/src/pentagon/action/Search.java
@@ -17,29 +17,29 @@ public class Search implements Action {
 	@Override
 	public String perform(HttpServletRequest request,
 			HttpServletResponse response) {
-		
-		
-		String keyWord = request.getParameter("key");
-		if(keyWord==null||keyWord.isEmpty()){
-			keyWord="travel";
-		} 
-		
-		FlickrBean flkBean = new FlickrBean();
-		flkBean.setAPIKey("8e2749644cb6405b3ee6a2c7b5f73eef");
-		flkBean.setBaseUrl("http://api.flickr.com/services/rest/");
-		flkBean.setMethod("flickr.photos.search");
-		flkBean.setPerPage("50");
-		flkBean.setFormat("json");
-		flkBean.setFlickrText(keyWord);
-		// flkBean.setFlickrContent_type("1");
-		// flkBean.setFlickrLat("37.779");
-		// flkBean.setFlickrLon("-122.420");
-		flkBean.setFlickrSort("interestingness-desc");
-		
-		FlickrAPI flkAPI = new FlickrAPI(flkBean);
-		JsonFlickrApi jfa = flkAPI.getFlickrImage();
 
-		request.setAttribute("flk_plist", jfa.photos.photo);
+		String keyWord = request.getParameter("key");
+		if (keyWord == null || keyWord.isEmpty()) {
+			keyWord = "travel";
+		}
+
+//		FlickrBean flkBean = new FlickrBean();
+//		flkBean.setAPIKey("8e2749644cb6405b3ee6a2c7b5f73eef");
+//		flkBean.setBaseUrl("http://api.flickr.com/services/rest/");
+//		flkBean.setMethod("flickr.photos.search");
+//		flkBean.setPerPage("50");
+//		flkBean.setFormat("json");
+//		flkBean.setFlickrText(keyWord);
+//		// flkBean.setFlickrContent_type("1");
+//		// flkBean.setFlickrLat("37.779");
+//		// flkBean.setFlickrLon("-122.420");
+//		flkBean.setFlickrSort("interestingness-desc");
+//
+//		FlickrAPI flkAPI = new FlickrAPI(flkBean);
+//		JsonFlickrApi jfa = flkAPI.getFlickrImage();
+//
+//		request.setAttribute("flk_plist", jfa.photos.photo);
+		request.setAttribute("term", keyWord);
 
 		return "gallery.jsp";
 	}
