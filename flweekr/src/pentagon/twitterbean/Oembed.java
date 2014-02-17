35984a0774a45e140c45f0d8f4ea3be0602a8ffe
Zuohr <haoran.zuo@gmail.com>
2014-02-16 22:38:16 -0500

    update

--- flweekr/src/pentagon/twitterbean/Oembed.java
+++ flweekr/src/pentagon/twitterbean/Oembed.java
@@ -1,5 +1,8 @@
 package pentagon.twitterbean;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Oembed {
 	private String html;
 
@@ -10,5 +13,18 @@ public class Oembed {
 	public void setHtml(String html) {
 		this.html = html;
 	}
-	
+
+	public Oembed strip() {
+		if (html != null) {
+			Pattern p = Pattern.compile("<p>.*</a>");
+			Matcher m = p.matcher(html);
+			if (m.find()) {
+				html = m.group();
+			}
+			return this;
+		} else {
+			return null;
+		}
+	}
+
 }
